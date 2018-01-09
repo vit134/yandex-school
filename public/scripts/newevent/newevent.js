6f3fadf8a4ce3761f6d905c194baa2317b15c22f
Vitaliy Andryushkov <vandryushkov@rbc.ru>
2018-01-09 18:45:43 +0300

    dropdown list livesearch

--- public/scripts/newevent/newevent.js
+++ public/scripts/newevent/newevent.js
@@ -3,16 +3,22 @@ import SimpleBar from 'SimpleBar';
 console.log('newevent');
 
 var $dropdowmContainer = $('#js-dropdown-container'),
-    $dropdowmItem = $('.js-dropdown-item'),
-    $dropdowmInput = $('.js-dropdown-input');
+    $dropdownItem = $('.js-dropdown-item'),
+    $dropdowmInput = $('.js-dropdown-input'),
+    $dropdownSelect = $('.js-newevent-select'),
+    $dropdownSelectOption = $dropdownSelect.find('option'),
+    $membersItem = $('.js-members-item'),
+    $removeMemberBtn = $('.js-remove-member');
+
+var scrollBar;
 
 function init() {
+    bindEvents();
 
-    var scrollBar = new SimpleBar($dropdowmContainer[0], {
+    scrollBar = new SimpleBar($dropdowmContainer[0], {
         autoHide: false
     });
 
-    bindEvents();
 }
 
 function bindEvents() {
@@ -27,11 +33,58 @@ function bindEvents() {
         }
     });
 
-    $dropdowmItem.on('click', function(e) {
+    $dropdownItem.on('click', function(e) {
         e.preventDefault();
-
-        console.log('click');
+        getMember($(this).data('id'));
+        $(this).addClass('hidden')
     });
+
+    $removeMemberBtn.on('click', function(e) {
+        e.preventDefault();
+        removemembers($(this).parent().data('id'));
+        $(this).parent().addClass('hidden');
+    })
+
+    $dropdowmInput.on('click keyup paste', liveSearch);
+}
+
+function getMember(id) {
+    $(this).addClass('hidden');
+
+    $dropdownSelectOption.filter(function(){
+        return $(this).val() == id
+    }).attr('selected', 'selected');
+
+    $membersItem.filter(function(){
+        return $(this).data('id') == id
+    }).removeClass('hidden');
+
+    $dropdowmContainer.removeClass('active');
+    $dropdowmInput.val('');
+}
+
+function removemembers(id) {
+    $dropdownSelectOption.filter(function(){
+        return $(this).val() == id
+    }).attr('selected', false);
+
+    $dropdownItem.filter(function(){
+        return $(this).data('id') == id
+    }).removeClass('hidden');
+}
+
+function liveSearch() {
+    var filter = $dropdowmInput.val();
+
+    $dropdownItem.each(function(){
+        if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+            $(this).fadeOut();
+        } else {
+            $(this).show();
+        }
+    });
+
+    scrollBar.recalculate();
 }
 
 init();
