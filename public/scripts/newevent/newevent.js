e20bd8dccd8510d66f126ef5668ead05a4637222
Vitaliy Andryushkov <vandryushkov@rbc.ru>
2018-01-09 14:31:46 +0300

    changes

--- public/scripts/newevent/newevent.js
+++ public/scripts/newevent/newevent.js
@@ -2,9 +2,39 @@ import SimpleBar from 'SimpleBar';
 
 console.log('newevent');
 
-var scrollBar = new SimpleBar(document.getElementById('js-scroll-container'), {
-    autoHide: false
-});
+var $dropdowmContainer = $('#js-dropdown-container'),
+    $dropdowmItem = $('.js-dropdown-item'),
+    $dropdowmInput = $('.js-dropdown-input');
+
+function init() {
+
+    var scrollBar = new SimpleBar($dropdowmContainer[0], {
+        autoHide: false
+    });
+
+    bindEvents();
+}
+
+function bindEvents() {
+
+    $dropdowmInput.on('click', function() {
+        $dropdowmContainer.addClass('active');
+    })
+
+    $(document).mouseup(function(e) {
+        if (!$dropdowmContainer.is(e.target) && $dropdowmContainer.has(e.target).length === 0) {
+            $dropdowmContainer.removeClass('active');
+        }
+    });
+
+    $dropdowmItem.on('click', function(e) {
+        e.preventDefault();
+
+        console.log('click');
+    });
+}
+
+init();
 
 
 
